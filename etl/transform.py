bb895197177e82fb92959459a9ae78616cc901fc
mwaimichael <michael.k.mwai@gmail.com>
2026-04-17 23:35:31 +0300

    added wind element

--- etl/transform.py
+++ etl/transform.py
@@ -10,7 +10,8 @@ def transform():
         "weather_description": data.get("weather")[0].get("description"),
         "temperature": data.get("main").get("temp"),
         "humidity(%)": data.get("main").get("humidity"),
-        "pressure(hpa)": data.get("main").get("grnd_level")
+        "pressure(hpa)": data.get("main").get("grnd_level"),
+        "wind_speed": data.get("wind").get("speed")
     }
 
 
